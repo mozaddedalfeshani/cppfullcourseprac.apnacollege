81f52cd4f372b1c6845b346d8e3d235b820dd306
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-09-23 21:18:36 +0600

    adding some in prime cpp

--- prime.cpp
+++ prime.cpp
@@ -1,37 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
     // variable declaration
 
     int input;
-    int i,j;
+    int i, j;
     bool detect = false;
-
 
     // taking Input from User
 
-    cin>> input;
-
+    cin >> input;
 
     // loop start for cheaking number
 
-    for(i=2;i<input;i++){
-        if(input%i==0){
+    for (i = 2; i < input; i++)
+    { /// Here you can use an alternative see below
+        if (input % i == 0)
+        {
             detect = true;
             break;
         }
-        else{
+        else
+        {
             detect = false;
         }
     }
-    if(detect==true){
-        cout<<" It's Not prime"<<endl;
+    if (detect == true)
+    {
+        cout << " It's Not prime" << endl;
     }
-    else{
-        cout<< "Its prime number:"<<endl;
+    else
+    {
+        cout << "Its prime number:" << endl;
     }
-
 
     return 0;
 }
+
+/*
+
+adding header file must
+#include <math.h>
+ for (i = 2; i < sqrt(input); i++)
+
+you can use also this style for more agrechieve style follow
+
+
+ */
